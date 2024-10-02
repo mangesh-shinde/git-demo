44b7208b1fde40dc8a07fa2a19a4d7a79a8e8c02
Mangesh Shinde <mangeshshinde793@gmail.com>
2024-10-02 16:36:14 +0530

    initial commit

---
+++ cpp/const_demo.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int scale_by(int num, int factor);    // forward declaration
+
+int main(){
+    const int factor = 3;
+    int num = 5;
+
+    std::cout << num << " scaled by " << factor << " : " << scale_by(num, factor) << "\n"; 
+    return 0;
+}
+
+int scale_by(int num, const int factor){
+    // factor = 10;         Compile time error as we are trying to modify const value
+    return num * factor;
+}
+
