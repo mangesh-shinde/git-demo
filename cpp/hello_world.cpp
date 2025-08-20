820bb233c08cdc1d1b858574f216e4428a8a8c3e
Mangesh Shinde <mangeshshinde793@gmail.com>
2025-08-20 10:26:22 +0530

    added first commit
    added second commit
    
    added third commit
    
    added fourth commit

--- cpp/hello_world.cpp
+++ cpp/hello_world.cpp
@@ -2,5 +2,9 @@
 
 int main(){
     std::cout << "Hello World\n";
+    std::cout << "This is first commit\n";
+    std::cout << "This is second commit\n";
+    std::cout << "This is third commit\n";
+    std::cout << "This is fourth commit\n";
     return 0;
 }
